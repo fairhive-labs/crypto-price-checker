488cb3a0d9aa0b92ac6a5ae8f3add34429a651e4
_whyvrafvr <jsie@trendev.fr>
2024-11-07 13:37:16 +0100

    display BTC price in USD & EUR

--- main.go
+++ main.go
@@ -21,38 +21,47 @@ type PriceData struct {
 		USD struct {
 			Rate string `json:"rate"`
 		} `json:"USD"`
+		EUR struct {
+			Rate string `json:"rate"`
+		} `json:"EUR"`
 	} `json:"bpi"`
 }
 
-// getPrice fetches the current price of a cryptocurrency from Coindesk API.
-func getPrice(c string) (float64, error) {
-	url := fmt.Sprintf("https://api.coindesk.com/v1/bpi/currentprice/%s.json", c)
+// getPrice fetches the current price of Bitcoin in USD and EUR from Coindesk API.
+func getPrice() (float64, float64, error) {
+	url := "https://api.coindesk.com/v1/bpi/currentprice.json"
 	resp, err := http.Get(url)
 	if err != nil {
-		return 0, err
+		return 0, 0, err
 	}
 	defer resp.Body.Close()
 
 	var d PriceData
 	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-		return 0, err
+		return 0, 0, err
 	}
 
-	// Remove commas from the price string and convert it to float64.
-	ps := strings.ReplaceAll(d.Bpi.USD.Rate, ",", "")
-	p, err := strconv.ParseFloat(ps, 64)
+	// Remove commas from the price strings and convert them to float64.
+	psUSD := strings.ReplaceAll(d.Bpi.USD.Rate, ",", "")
+	pUSD, err := strconv.ParseFloat(psUSD, 64)
 	if err != nil {
-		return 0, err
+		return 0, 0, err
 	}
 
-	return p, nil
+	psEUR := strings.ReplaceAll(d.Bpi.EUR.Rate, ",", "")
+	pEUR, err := strconv.ParseFloat(psEUR, 64)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	return pUSD, pEUR, nil
 }
 
 func main() {
-	var l float64 // Store the last fetched price
+	var lastUSD float64 // Store the last fetched USD price
 
 	for {
-		p, err := getPrice("BTC")
+		pUSD, pEUR, err := getPrice()
 		if err != nil {
 			fmt.Println("Error fetching price:", err)
 			continue
@@ -61,9 +70,9 @@ func main() {
 		col := n        // Default color (normal)
 		var pct float64 // Percentage change between prices
 
-		if l != 0 { // If we have a previous price to compare with
-			diff := p - l
-			pct = (diff / l) * 100
+		if lastUSD != 0 { // If we have a previous USD price to compare with
+			diff := pUSD - lastUSD
+			pct = (diff / lastUSD) * 100
 
 			if diff > 0 {
 				col = g // Price increased: use green color
@@ -72,9 +81,9 @@ func main() {
 			}
 		}
 
-		fmt.Printf("BTC Price: %s$%.2f (%.2f%%)%s\n", col, p, pct, n)
+		fmt.Printf("BTC Price: %s$%.2f (%.2f%%) | €%.2f%s", col, pUSD, pct, pEUR, n)
 
-		l = p // Update lastPrice for next iteration
+		lastUSD = pUSD // Update last USD price for next iteration
 
 		time.Sleep(10 * time.Second) // Wait for 10 seconds before refreshing
 	}
